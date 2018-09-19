09601bb517c87fcbf13b863de6b2b372dcf990f7
Alessandro Marzialetti <ale.marzialetti@gmail.com>
2018-09-19 16:36:49 +0200

    added strcat

---
+++ src/strcat.s
@@ -0,0 +1,24 @@
+section .text
+
+global _ft_strcat
+
+_ft_strcat:
+	;rdi, rsi
+	push rdi
+	push rsi
+l1:
+	cmp byte [rdi], 0
+	jz l2
+	inc rdi
+	jmp l1
+l2:
+	cmp byte [rsi], 0
+	jz done
+	movsb
+	jmp l2
+done:
+	mov byte [rdi], 0
+	pop rsi
+	pop rdi
+	mov rax, rdi
+	ret
